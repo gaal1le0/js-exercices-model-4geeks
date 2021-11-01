8a2333f958f0f5f1c077fdcd02da3ceacb5b3bec
ManuuMT <ema14mt@gmail.com>
2021-11-01 16:43:14 +0000

    Ejercicios 1 y 2 resueltos

--- index.js
+++ index.js
@@ -2,3 +2,61 @@
 let undefined;
 const sequence = [10, 4, 8, 27.4, 500, null, undefined, 100, 25, 40, 31, 20, 17, 1890, 13, 42];
 const letrasDNI = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+
+
+function cleanArray(myArray){
+    let arrayFixed = [];
+    for(let i in myArray){
+        if(typeof myArray[i]=='number') arrayFixed.push(myArray[i]);
+    }
+    return arrayFixed;
+}
+
+
+function sortArray(myArray,symbol){
+    let arr = cleanArray(myArray);
+    
+    if(symbol=="<" || symbol==">"){
+        for(let i in arr){
+            auxSort(i,arr,symbol);
+        }
+    }
+    else return ("Símbolo incorrecto. Inténtelo más tarde (:");
+    
+    return arr;
+}
+
+function auxSort(i,arr,symbol){
+    for (let j=0; j<i; j++){
+        choiceHowToSort(i,j,arr,symbol);
+    }
+}
+
+function choiceHowToSort(i,j,arr,symbol){
+    if(symbol=="<"){
+        lowestToHighest(i,j,arr);  
+    }
+
+    if(symbol==">") {
+        highestToLowest(i,j,arr);   
+    }
+}
+
+function lowestToHighest(i,j,arr){
+    if(arr[i]<arr[j]){
+        let aux = arr[i];
+        arr[i]=arr[j];
+        arr[j]=aux;
+    }
+}
+function highestToLowest(i,j,arr){
+    if(arr[i]>arr[j]){
+        let aux = arr[i];
+        arr[i]=arr[j];
+        arr[j]=aux;
+    }
+}
+
+console.log(sortArray(sequence,"<"));
+
+
